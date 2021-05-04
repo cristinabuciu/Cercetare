551fdb1a016eea2a59dd6ca813a5e6d1992abb55
ClaudiuNedelcu1307 <claudiu.nedelcu.m@gmail.com>
2021-05-04 14:05:10 +0300

    remove Home page

--- DataDeposit/static/src/appRoutes.tsx
+++ DataDeposit/static/src/appRoutes.tsx
@@ -28,7 +28,8 @@ export default class AppRoutes extends Component {
         
         return (
             <Switch >
-                <Route path="/" exact render={(props) => <Home {...props} greeting={""} />}/>
+                {/* <Route path="/" exact render={(props) => <Home {...props} greeting={""} />}/> */}
+                <Route path="/" exact render={(props) => <SearchPage {...props} greeting={""} />}/>
                 <Route path="/LoginForm" exact render={(props) => <LoginForm {...props} color={"black"} />} />
                 <Route path="/search" exact render={(props) => <SearchPage {...props} greeting={""} />}/>
                 <Route path="/uploadPage" exact render={(props) => <UploadPage {...props} color={"UPLOAD "} />} />
