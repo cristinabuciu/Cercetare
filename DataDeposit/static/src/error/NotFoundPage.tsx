e41131ff65f0adaee8cdbd9e16836810d1aed9ed
ClaudiuNedelcu1307 <claudiu.nedelcu.m@gmail.com>
2021-06-16 22:21:21 +0300

    replace-it

--- DataDeposit/static/src/error/NotFoundPage.tsx
+++ DataDeposit/static/src/error/NotFoundPage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MyTranslator from '../assets/MyTranslator';
+
 class NotFoundPage extends React.Component{
-    render(){
-        return <div>
-            
+    render(){ 
+        const translate = new MyTranslator("common");
+        return (
+          <div>
             <p style={{textAlign:"center"}}>
             <img src={'static/dist/content/images/404.png'}  />
             <br />
-              <Link to="/">Go to Home </Link>
+              <Link to="/">{translate.useTranslation('Go-Home')}</Link>
             </p>
-          </div>;
+          </div>
+        );
     }
 }
 export default NotFoundPage;
